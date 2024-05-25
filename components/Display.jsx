0b23345cdb49c518d9f2e83b679a4e50fe33dcb6
martinsharma17 <martinsharma18@gmail.com>
2024-05-25 23:56:49 +0545

    sec

--- components/Display.jsx
+++ components/Display.jsx
@@ -1,13 +1,13 @@
 import './Display.css'
 
-function Display({time}) {
+function Display({ time }) {
 
-    function formatTime(timeInSecs){
-        const hours = Math.floor(timeInSecs/3600);
-        const minutes = Math.floor((timeInSecs % 3600)/60);
+    function formatTime(timeInSecs) {
+        const hours = Math.floor(timeInSecs / 3600);
+        const minutes = Math.floor((timeInSecs % 3600) / 60);
         const seconds = Math.floor(timeInSecs % 60);
 
-        return{
+        return {
             hours,
             minutes,
             seconds
@@ -20,19 +20,19 @@ function Display({time}) {
 
                 <div className="hours">
                     <span className="Number">
-                    {String(formatTime(time).hours).padStart(2,"0")}
-                    :</span>
+                        {String(formatTime(time).hours).padStart(2, "0")}
+                        :</span>
                     <label>Hours</label>
                 </div>
 
                 <div className="minutes">
                     <span className="Number">
-                    {String(formatTime(time).minutes).padStart(2,"0")}:</span>
+                        {String(formatTime(time).minutes).padStart(2, "0")}:</span>
                     <label>minutes</label>
                 </div>
 
                 <div className="secondd">
-                    <span className="Number">  {String(formatTime(time).seconds).padStart(2,"0")}:</span>
+                    <span className="Number">  {String(formatTime(time).seconds).padStart(2, "0")}</span>
                     <label>seconds</label>
                 </div>
 
